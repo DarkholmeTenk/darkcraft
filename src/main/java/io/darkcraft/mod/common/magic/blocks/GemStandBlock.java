e9af446fbd503818716e7a2a03415713b1497d4d
DarkholmeTenk <shaney.booth@gmail.com>
2016-06-22 07:35:46 +0100

    Added recipes and name for gem stand

--- src/main/java/io/darkcraft/mod/common/magic/blocks/GemStandBlock.java
+++ src/main/java/io/darkcraft/mod/common/magic/blocks/GemStandBlock.java
@@ -1,11 +1,15 @@
 package io.darkcraft.mod.common.magic.blocks;
 
+import cpw.mods.fml.common.registry.GameRegistry;
 import io.darkcraft.darkcore.mod.abstracts.AbstractBlockContainer;
 import io.darkcraft.mod.DarkcraftMod;
 import io.darkcraft.mod.client.renderer.tileent.GemStandRenderer;
 import io.darkcraft.mod.common.magic.blocks.tileent.GemStand;
+import io.darkcraft.mod.common.magic.items.MagicComponent;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 
 public class GemStandBlock extends AbstractBlockContainer
 {
@@ -31,13 +35,16 @@ public class GemStandBlock extends AbstractBlockContainer
 	{
 		setBlockName("GemStand");
 		setBlockBounds(0.4f,0,0.4f,0.6f,0.9f,0.6f);
+		setHardness(5.0f);
 	}
 
 	@Override
 	public void initRecipes()
 	{
-		// TODO Auto-generated method stub
-
+		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(this), false, " g ","nin","nin",
+				'g', MagicComponent.Type.Crystal.getIS(1),
+				'n', "nuggetGold",
+				'i', "ingotIron"));
 	}
 
 	@Override
