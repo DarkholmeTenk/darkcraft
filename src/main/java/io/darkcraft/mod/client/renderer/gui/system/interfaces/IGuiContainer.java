a7a5d172aedeaea56278b2ac28d6d50f50465a6e
DarkholmeTenk <shaney.booth@gmail.com>
2016-07-03 01:27:03 +0100

    Added extra methods to IGuiContainer

--- src/main/java/io/darkcraft/mod/client/renderer/gui/system/interfaces/IGuiContainer.java
+++ src/main/java/io/darkcraft/mod/client/renderer/gui/system/interfaces/IGuiContainer.java
@@ -1,14 +1,19 @@
 package io.darkcraft.mod.client.renderer.gui.system.interfaces;
 
+import io.darkcraft.darkcore.mod.datastore.WindowSpaceStore;
 import io.darkcraft.mod.client.renderer.gui.system.AbstractGuiElement;
 
 public interface IGuiContainer
 {
 	public void addElement(AbstractGuiElement e);
 
+	public void removeElement(AbstractGuiElement e);
+
 	public void clickableClicked(IClickable c, String id);
 
 	public void typableChanged(ITypable t);
 
 	public void recalc();
+
+	public WindowSpaceStore getWindowSpace(AbstractGuiElement e);
 }
