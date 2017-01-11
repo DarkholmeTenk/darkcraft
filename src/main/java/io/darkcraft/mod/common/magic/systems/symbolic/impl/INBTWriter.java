b3f45135ed06c92e61c2d82321fbcd3ebe6a7196
DarkholmeTenk <shaneybooth@gmail.com>
2017-01-11 17:17:25 +0000

    Removed soul spells and current symbolics

--- src/main/java/io/darkcraft/mod/common/magic/systems/symbolic/impl/INBTWriter.java
+++
@@ -1,8 +0,0 @@
-package io.darkcraft.mod.common.magic.systems.symbolic.impl;
-
-import net.minecraft.nbt.NBTTagCompound;
-
-public interface INBTWriter
-{
-	public void writeToNBT(NBTTagCompound nbt);
-}
